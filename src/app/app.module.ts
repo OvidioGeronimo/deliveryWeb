49ff6294987962a9f88d9ad33c88aab2655cdcf3
OVIDIO GERONIMO <ovidio.geronimo@senacsp.edu.br>
2019-08-26 22:17:16 -0300

    update/remove-img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { CriarContaComponent } from './usuarios/criar-conta/criar-conta.component';
+import { EsqueciSenhaComponent } from './usuarios/esqueci-senha/esqueci-senha.component';
+import { LoginComponent } from './usuarios/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ToastrModule } from 'ngx-toastr';
@@ -14,12 +17,13 @@ import { HomeLayoutComponent } from './layout/home-layout/home-layout.component'
 import { LoginLayoutComponent } from './layout/login-layout/login-layout.component';
 import { FormCategoriasComponent } from './categorias/form-categorias/form-categorias.component';
 import { ListaCategoriasComponent } from './categorias/lista-categorias/lista-categorias.component';
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
 import { AngularFireDatabaseModule} from '@angular/fire/database';
 import { AngularFireStorageModule} from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { ListaProdutosComponent } from './produtos/lista-produtos/lista-produtos.component';
 import { FormProdutosComponent } from './produtos/form-produtos/form-produtos.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 @ NgModule({
   declarations: [
@@ -32,7 +36,10 @@ import { FormProdutosComponent } from './produtos/form-produtos/form-produtos.co
     FormCategoriasComponent,
     ListaCategoriasComponent,
     ListaProdutosComponent,
-    FormProdutosComponent
+    FormProdutosComponent,
+    LoginComponent,
+    EsqueciSenhaComponent,
+    CriarContaComponent,
   ],
   imports: [
     BrowserModule,
@@ -43,6 +50,7 @@ import { FormProdutosComponent } from './produtos/form-produtos/form-produtos.co
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireStorageModule,
+    AngularFireAuthModule,
     ToastrModule.forRoot()
 
   ],
